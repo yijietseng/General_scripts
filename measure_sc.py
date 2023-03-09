0bf10f14cc74adf343da8351133ff55172052ef8
Yi Jie Tseng <yijietseng@gmail.com>
2023-03-08 20:04:50 -0700

    Add files via upload
    sync

--- measure_sc.py
+++ measure_sc.py
@@ -1,90 +1,149 @@
 from pyrosetta import *
 from pyrosetta.rosetta.protocols.simple_filters import ShapeComplementarityFilter
-import sys,glob
+import sys, glob, argparse, os
 from sys import argv
 init()
 
-if '-h' in argv:
-	Description = '''
-	tag			argument		Description
-	-m			<No_arg>		Execute multi-structure measurement. This method can only do structure names in series 
-	-l			<No_arg>		Measure the SC of the ligand against the complex
-	-r			<path>			To check if record the SC into a log. Argument is the path of the output logfile
-	-p			<path>			input the PDB structure. For multiple structure mode, input only the path to the structure directory
-	'''
-	print(Description)
-	sys.exit()
+def get_residue_ls(pose, chain='A'):
 
-
-if '-p' in argv:
-	PDBname = argv[argv.index('-p')+1]
-
-if '-m' in argv:
-	if '-r' in argv:
-		if '-p' not in argv:
-			print('Please input a path to the PDB structure!!!')
+	total = pose.total_residue()
+	ligstr=''
+	enzstr=''
+	for i in range(1,total+1):
+		resi_info = pose.pdb_info().pose2pdb(i)
+		resi = resi_info.replace(' ','')
+		if resi[-1] == chain:
+			ligstr += resi+','
 		else:
-			strucNo = argv.index('-m')+1
-			logpath = argv[argv.index('-r')+1]
-			PDB = glob.glob(PDBname+'*.pdb')
-			f = open(logpath,'a+')
-			f.write('PDBFN\tSC\n')
-			sc = ShapeComplementarityFilter()
-			SCls = []
-			for i in PDB:
-				pose = pose_from_pdb(i)
-				sc_score = sc.score(pose)
-				f.write(i+'\t'+str(sc_score)+'\n')
-				SCls.append(i+':'+str(sc_score))
-			f.close()
-			print(SCls)
-	else:
-		if '-p' not in argv:
-			print('Please input a path to the PDB structure!!!')
-		else:
-			strucNo = argv.index('-m')+1
-			logpath = argv[argv.index('-r')+1]
-			PDB = glob.glob(PDBname+'*.pdb')
-			sc = ShapeComplementarityFilter()
-			SCls = []
-			for i in PDB:
-				pose = pose_from_pdb(i)
-				sc_score = sc.score(pose)
-				SCls.append(i+':'+str(sc_score))
-			print(SCls)
-else:
-	if '-p' not in argv:
-		print('Please input a path to the PDB structure!!!')
-	else:
-		sc = ShapeComplementarityFilter()
-		pose = pose_from_pdb(PDBname)
-		sc_score = sc.score(pose)
-		print('SC score for '+PDBname+' is:'+str(sc_score))
-if '-l' in argv:
-	if '-p' not in argv:
-		print('Please input a path to the PDB structure!!!')
-	else:
-		pose = pose_from_pdb(PDBname)
-		
-		total = pose.total_residue()
+			enzstr += resi+','
+	ligstr = ligstr[:-1]
+	enzstr = enzstr[:-1]
 
-		sc = ShapeComplementarityFilter()
-		
-		ligstr=''
-		enzstr=''
-		for i in range(1,total+1):
-			resi_info = pose.pdb_info().pose2pdb(i)
-			resi = resi_info.replace(' ','')
-			if resi[-1] == 'A':
-				ligstr += resi+','
-			else:
-				enzstr += resi+','
-		ligstr = ligstr[:-1]
-		enzstr = enzstr[:-1]
-		
+	return (ligstr, enzstr)
+
+
+def measrue_sc(pose, ligstr='', enzstr=''):
+	sc = ShapeComplementarityFilter()
+
+	# if a ligand is identified, it measure the ligand's sc. Otherwise, it measures global sc
+	if ligstr and enzstr:
 		sc.residues1(ligstr)
 		sc.residues2(enzstr)
-		sc_score = sc.score(pose)
-		print('SC score for '+PDBname+' is:'+str(sc_score))
+	
+	sc_score = sc.score(pose)
+	
+	return sc_score
 
 
+def ligand_sc(PDBname, chain='A', logpath=''):
+	pose = pose_from_pdb(PDBname)
+		
+	ligstr, enzstr = get_residue_ls(pose, chain)
+	sc_score = measrue_sc(pose, ligstr, enzstr)
+
+	if logpath:
+		f = open(logpath,'a+')
+		f.write('PDBFN\tSC\n')
+		f.write(PDBname+'\t'+str(sc_score)+'\n')
+		f. close()
+		print(f'log is saved to {logpath}')
+
+	print('SC score for '+PDBname+' is:'+str(sc_score))
+
+
+def multi_sc(PDBpath, logpath=''):
+	'''
+	Measures SC for multiple structures.
+	'''
+
+	PDB = glob.glob(PDBpath+'*.pdb')
+	if logpath:
+		f = open(logpath,'a+')
+		f.write('PDBFN\tSC\n')
+
+	SCls = []
+	for i in PDB:
+		pose = pose_from_pdb(i)
+		sc_score = measrue_sc(pose)
+		if logpath:
+			f.write(i+'\t'+str(sc_score)+'\n')
+		SCls.append(i+':'+str(sc_score))
+	if logpath:
+		f.close()
+	print(SCls)
+
+
+def single_sc(PDBname, logpath=''):
+	pose = pose_from_pdb(PDBname)
+	sc_score = measrue_sc(pose)
+	if logpath:
+		f = open(logpath,'a+')
+		f.write('PDBFN\tSC\n')
+		f.write(PDBname+'\t'+str(sc_score)+'\n')
+		f.close()
+
+	print('SC score for '+PDBname+' is:'+str(sc_score))
+
+#-------------------------Below section is definitions of input options------------------------------------------
+
+parser = argparse.ArgumentParser(description='Measure the SC score for 1 or multiple structures, it can also measure the SC for just the Ligand')
+parser.add_argument("-i", 
+                    '--input',
+                    nargs=1,
+                    type=str,
+                    required=True,
+                    help='input PDB file path/name')
+parser.add_argument('-m',
+                    '--multi', 
+					action='store_true',
+                    help='Measure SC for multiple structures')
+parser.add_argument('-l',
+                    '--ligand',
+                    nargs='?',
+		    		const='A',
+                    type=str,
+                    help='Measure SC of ligand, argument is the ChainID of the ligand, defualt = A')
+parser.add_argument('-r',
+                    '--record',
+                    nargs=1,
+		    		type=str,
+                    help='Record a log file or not')
+parser.add_argument('-s',
+                    '--single', 
+                    action='store_true',
+                    help='Measure SC for a single structure')
+
+args = parser.parse_args()
+
+#---------------------------------------------Section ends----------------------------------------------------
+
+
+def main():
+	PDB = args.input[0]
+	
+	if args.record:
+		logpath = args.record[0]
+	else:
+		logpath = ''
+
+	if args.single:
+		if os.path.isdir(PDB):
+			raise ValueError('Please input a path to a single PDB file')
+		else:
+			single_sc(PDB, logpath)
+	elif args.multi:
+		if os.path.isdir(PDB):
+			multi_sc(PDB, logpath)
+		else:
+			raise ValueError('Please input a path to multiple PDB file')
+	
+	elif args.ligand:
+		if os.path.isdir(PDB):
+			raise ValueError('Please input a path to a single PDB file with ligand')
+		else:
+			ligand_sc(PDB, args.ligand[0],logpath)
+	
+
+
+if __name__ == '__main__':
+	main()
--- relax.py
+++ relax.py
@@ -1,81 +1,170 @@
-#----------------------------Start timer--------------------------------
+#-------------------------Set up Pyrosetta------------------------------
 import time
 from sys import argv
-start = time.time()
-
-#-------------------------Set up Pyrosetta------------------------------
 from pyrosetta.rosetta import *
 from pyrosetta import *
 from pyrosetta.toolbox import *
-from pyrosetta.rosetta.core.scoring import fa_rep, atom_pair_constraint
-from pyrosetta.rosetta.core.pack.task.operation import ReadResfile, RestrictToRepacking
+from pyrosetta.rosetta.core.scoring import atom_pair_constraint
 from pyrosetta.rosetta.protocols.relax import FastRelax
-import itertools
-init('-beta_nov16 True')
+import itertools, os, argparse, glob
+import multiprocessing as mp
 
-PDB = argv[1]
+#init('-beta_nov16 True')
+init()
 
+def format_outputFN(pdbname):
+    if '_R' in pdbname:
+        pdbname = pdbname.split('.pdb')[0][:-2]
+        R_prefix = ''
+    else:
+        pdbname = pdbname.split('.pdb')[0]
+        R_prefix = '_R'
 
-for i in itertools.count(start=1):
-    if not os.path.exists(PDB[:-4]+'_R'+"{:02d}".format(i)+'.pdb'):
-        if i > 1:
-            file_incre = i-1
-        else:
-            file_incre = i
-        break
+    for i in itertools.count(start=1):
+        if not os.path.exists(pdbname+R_prefix+"{:02d}".format(i)+'.pdb'):
+            outputFN = pdbname+R_prefix+"{:02d}".format(i)+'.pdb'
+            print(f'\noutput file is set to {outputFN}\n')
+            break
 
-outputFN = PDB[:-4]+'_R'+"{:02d}".format(file_incre)+'.pdb'
+    return outputFN
 
-
-No_list = ['n','no']
-
-
-pose = pose_from_pdb(PDB)
-scorefxn = create_score_function('beta_nov16')
-
-# Check if constraint file is needed
-cst_check = input('\nDo you wish to use a contraint file?(y/n):')
-cst_check = cst_check.lower()
-
-if cst_check in No_list:
-    print('No constraint file applied!!!!')
-else:
-    constraint_file = input('Please enter a path for the constraint file:')
+def set_cst(sf, pose):
+    constraint_file = input('Please enter a path for the constraint file:').strip()
     constraint = pyrosetta.rosetta.protocols.constraint_movers.ConstraintSetMover()
     constraint.constraint_file(constraint_file)
     constraint.add_constraints(True)
     constraint.apply(pose)
-    scorefxn.set_weight(atom_pair_constraint, 1)
+    sf.set_weight(atom_pair_constraint, 1)
 
 
-score_before = scorefxn(pose)
+def set_movemap(set_bb=True, set_chi=True, set_jump=False):
+    movemap = MoveMap()
+    movemap.set_bb(set_bb) 
+    movemap.set_chi(set_chi)
+    movemap.set_jump(set_jump)
+    return movemap
 
-movemap = MoveMap()
-movemap.set_bb(True) 
-movemap.set_chi(True)
+def RunRelax(sf, mmp, pose, apply=True):
+    relax = FastRelax()
+    relax.set_scorefxn(sf)
+    relax.set_movemap(mmp)
+    if apply:
+        relax.apply(pose)
+        return pose
+    else:
+        print('\npassed FastRelax Mover\n')
+    
 
-relax = FastRelax()
-relax.set_scorefxn(scorefxn)
-relax.set_movemap(movemap)
-relax.apply(pose)
+def cal_wall_time(start, end):
+    d = end-start
+    if d >= 3600:
+        print ('\n'*2, 'The time spent on relaxing with constraints was:', d/3600, 'hours')
+    elif 60 <= d < 3600:
+        print ('\n'*2, 'The time spent on relaxing with constraints was:', d/60, 'minutes')
+    elif d < 60:
+        print ('\n'*2, 'The time spent on relaxing with constraints was:', d, 'seconds')
+    return d
 
 
-#-----------------------------Out put-----------------------------------
-score_after = scorefxn(pose)
+def RunProcess(PDB, cst=''):
 
-print ('\n'*2, 'The initial score of this structure is:', '\n', score_before)
-print ('The score after relaxing is:', '\n', score_after)
-print ('\n','The distribution of scores', '\n')
-scorefxn.show(pose)
+    pose = pose_from_pdb(PDB)
+    outputFN = format_outputFN(PDB)
+    #scorefxn = create_score_function('beta_nov16')
+    scorefxn = get_fa_scorefxn()
 
-pose.dump_pdb(outputFN)
+    if cst:
+        set_cst(scorefxn, pose)
+    # Set movemap
+    mmp = set_movemap()
+    
+    # Run relax
+    score_before = scorefxn(pose)
+    pose_relaxed = RunRelax(scorefxn, mmp, pose, apply=True)
+    score_after = scorefxn(pose_relaxed)
+    
+    # output
+    print ('\n'*2, f'The initial score of {outputFN} structure is:', '\n', score_before)
+    print ('The score after relaxing is:', '\n', score_after)
+    print ('\n','The distribution of scores', '\n')
+    scorefxn.show(pose_relaxed)
 
-#----------------------------Wall time----------------------------------
-end = time.time()
-d = end-start
-if d >= 3600:
-	print ('\n'*2, 'The time spent on relaxing with constraints was:', d/3600, 'hours')
-if 60 <= d < 3600:
-	print ('\n'*2, 'The time spent on relaxing with constraints was:', d/60, 'minutes')
-if d < 60:
-	print ('\n'*2, 'The time spent on relaxing with constraints was:', d, 'seconds')
+    pose.dump_pdb(outputFN)
+
+    return f'{outputFN}\t{score_after}'
+
+#-------------------------Below section is definitions of input options------------------------------------------
+
+parser = argparse.ArgumentParser(description='Relax inputed structure')
+parser.add_argument('-c',
+                    '--constraint',
+                    nargs=1,
+                    type=str,
+                    help='Input a constraint file')
+parser.add_argument("-i", 
+                    '--input',
+                    nargs=1,
+                    type=str,
+                    required=True,
+                    help='input PDB file path/name')
+parser.add_argument('-m',
+                    '--multi', 
+					action='store_true',
+                    help='Relax multiple structures by multithreading')
+parser.add_argument('-r',
+                    '--record',
+                    nargs=1,
+		    		type=str,
+                    help='Record a log file or not')
+
+
+args = parser.parse_args()
+
+#---------------------------------------------Section ends----------------------------------------------------
+
+
+def main():
+    #----------------------------Start timer--------------------------------
+    start = time.time()
+    #----------------------------Main function------------------------------
+    if os.path.isfile(args.input[0]):
+        if args.constraint:
+            score = RunProcess(args.input[0], cst=args.constrain[0])
+        else:
+            score = RunProcess(args.input[0])
+
+        if args.record:
+            with open(args.record[0], 'a') as fin:
+                fin.write(score)
+        
+        end = time.time()
+        # Calculate wall time
+        cal_wall_time(start, end)
+
+    elif os.path.isdir(args.input[0]):
+
+        PDBls = glob.glob(args.input[0]+'*.pdb')
+
+        pool = mp.Pool(int(mp.cpu_count()*2/3))
+        if args.constraint:
+            cstls = []
+            for _ in range(len(PDBls)):
+                cstls.append(args.constraint[0])
+            combine_ls = list(zip(PDBls,cstls))
+            scores = pool.map(RunProcess, combine_ls)
+        else:
+            scores = pool.map(RunProcess, PDBls)
+
+        if args.record:
+            with open(args.record[0], 'a') as fin:
+                fin.write(scores)
+        
+
+        end = time.time()
+        # Calculate wall time
+        cal_wall_time(start, end)
+
+
+
+if __name__ == '__main__':
+    main()
