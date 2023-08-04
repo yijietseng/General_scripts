c700c7fd58bf90fa1275def68691c4a477cc2d55
Yi Jie Tseng <yijietseng@gmail.com>
2023-08-04 10:38:57 -0600

    update

--- relax.py
+++ relax.py
@@ -8,114 +8,149 @@ from pyrosetta.rosetta.core.scoring import atom_pair_constraint
 from pyrosetta.rosetta.protocols.relax import FastRelax
 import itertools, os, argparse, glob
 import multiprocessing as mp
+import numpy as np
 
 #init('-beta_nov16 True')
 init()
 
 def format_outputFN(pdbname):
-    if '_R' in pdbname:
-        pdbname = pdbname.split('.pdb')[0][:-2]
-        R_prefix = ''
-    else:
-        pdbname = pdbname.split('.pdb')[0]
-        R_prefix = '_R'
+	if '_R' in pdbname:
+		pdbname = pdbname.split('.pdb')[0][:-2]
+		R_prefix = ''
+	else:
+		pdbname = pdbname.split('.pdb')[0]
+		R_prefix = '_R'
 
-    for i in itertools.count(start=1):
-        if not os.path.exists(pdbname+R_prefix+"{:02d}".format(i)+'.pdb'):
-            outputFN = pdbname+R_prefix+"{:02d}".format(i)+'.pdb'
-            print(f'\noutput file is set to {outputFN}\n')
-            break
+	for i in itertools.count(start=1):
+		if not os.path.exists(pdbname+R_prefix+"{:02d}".format(i)+'.pdb'):
+			outputFN = pdbname+R_prefix+"{:02d}".format(i)+'.pdb'
+			print(f'\noutput file is set to {outputFN}\n')
+			break
 
-    return outputFN
+	return outputFN
 
-def set_cst(sf, pose):
-    constraint_file = input('Please enter a path for the constraint file:').strip()
-    constraint = pyrosetta.rosetta.protocols.constraint_movers.ConstraintSetMover()
-    constraint.constraint_file(constraint_file)
-    constraint.add_constraints(True)
-    constraint.apply(pose)
-    sf.set_weight(atom_pair_constraint, 1)
 
+def set_cst(sf, pose, cstfile):
+	constraint_file = cstfile
+	constraint = pyrosetta.rosetta.protocols.constraint_movers.ConstraintSetMover()
+	constraint.constraint_file(constraint_file)
+	constraint.add_constraints(True)
+	constraint.apply(pose)
+	sf.set_weight(atom_pair_constraint, 1)
+
+
+def gen_cst(poseName, filename, weight=0.25):
+
+	pose = pose_from_pdb(poseName)
+	
+	# Bind the CA of the backbone
+	# List out the residue indices
+	bkbone = np.arange(1,pose.total_residue()+1,dtype=int)
+
+	filepath = f'{filename}.cst'
+
+	c1 = open(filepath, "w")
+	# Loop through and only bind together residues that are at least 7 away in index, which typically follows the sequence
+	c1.write('\n# Connect the CA of the backbone together\n\n')
+	try:
+		for index in bkbone:
+			for ind in bkbone:
+				if (index-ind > 7):
+					dVec= pose.residue(index).xyz("CA")-pose.residue(ind).xyz("CA")
+					# We will see if it allows me to remove the spaces from the output of pose2pdb this easily
+					pdbind = pose.pdb_info().pose2pdb(ind).replace(' ','')
+					pdbindex = pose.pdb_info().pose2pdb(index).replace(' ','')
+					c1.write(f'AtomPair CA {pdbind} CA {pdbindex} FLAT_HARMONIC {dVec.norm()} {weight} {weight}\n')
+	except:
+		print('some residue has no CA')		
+	print(f'Constraint file: {filepath} written')
+		
 
 def set_movemap(set_bb=True, set_chi=True, set_jump=False):
-    movemap = MoveMap()
-    movemap.set_bb(set_bb) 
-    movemap.set_chi(set_chi)
-    movemap.set_jump(set_jump)
-    return movemap
+	movemap = MoveMap()
+	movemap.set_bb(set_bb) 
+	movemap.set_chi(set_chi)
+	movemap.set_jump(set_jump)
+	return movemap
 
 def RunRelax(sf, mmp, pose, apply=True):
-    relax = FastRelax()
-    relax.set_scorefxn(sf)
-    relax.set_movemap(mmp)
-    if apply:
-        relax.apply(pose)
-        return pose
-    else:
-        print('\npassed FastRelax Mover\n')
-    
+	relax = FastRelax()
+	relax.set_scorefxn(sf)
+	relax.set_movemap(mmp)
+	if apply:
+		relax.apply(pose)
+		return pose
+	else:
+		print('\npassed FastRelax Mover\n')
+	
 
 def cal_wall_time(start, end):
-    d = end-start
-    if d >= 3600:
-        print ('\n'*2, 'The time spent on relaxing with constraints was:', d/3600, 'hours')
-    elif 60 <= d < 3600:
-        print ('\n'*2, 'The time spent on relaxing with constraints was:', d/60, 'minutes')
-    elif d < 60:
-        print ('\n'*2, 'The time spent on relaxing with constraints was:', d, 'seconds')
-    return d
+	d = end-start
+	if d >= 3600:
+		print ('\n'*2, 'The time spent on relaxing with constraints was:', d/3600, 'hours')
+	elif 60 <= d < 3600:
+		print ('\n'*2, 'The time spent on relaxing with constraints was:', d/60, 'minutes')
+	elif d < 60:
+		print ('\n'*2, 'The time spent on relaxing with constraints was:', d, 'seconds')
+	return d
 
 
 def RunProcess(PDB, cst=''):
 
-    pose = pose_from_pdb(PDB)
-    outputFN = format_outputFN(PDB)
-    #scorefxn = create_score_function('beta_nov16')
-    scorefxn = get_fa_scorefxn()
+	pose = pose_from_pdb(PDB)
+	outputFN = format_outputFN(PDB)
+	#scorefxn = create_score_function('beta_nov16')
+	scorefxn = get_fa_scorefxn()
 
-    if cst:
-        set_cst(scorefxn, pose)
-    # Set movemap
-    mmp = set_movemap()
-    
-    # Run relax
-    score_before = scorefxn(pose)
-    pose_relaxed = RunRelax(scorefxn, mmp, pose, apply=True)
-    score_after = scorefxn(pose_relaxed)
-    
-    # output
-    print ('\n'*2, f'The initial score of {outputFN} structure is:', '\n', score_before)
-    print ('The score after relaxing is:', '\n', score_after)
-    print ('\n','The distribution of scores', '\n')
-    scorefxn.show(pose_relaxed)
+	if cst:
+		set_cst(scorefxn, pose, cst)
+	# Set movemap
+	mmp = set_movemap()
+	
+	# Run relax
+	score_before = scorefxn(pose)
+	pose_relaxed = RunRelax(scorefxn, mmp, pose, apply=True)
+	score_after = scorefxn(pose_relaxed)
+	
+	# output
+	print ('\n'*2, f'The initial score of {outputFN} structure is:', '\n', score_before)
+	print ('The score after relaxing is:', '\n', score_after)
+	print ('\n','The distribution of scores', '\n')
+	scorefxn.show(pose_relaxed)
 
-    pose.dump_pdb(outputFN)
+	pose.dump_pdb(outputFN)
 
-    return f'{outputFN}\t{score_after}'
+	return f'{outputFN}\t{score_after}'
 
 #-------------------------Below section is definitions of input options------------------------------------------
 
 parser = argparse.ArgumentParser(description='Relax inputed structure')
 parser.add_argument('-c',
-                    '--constraint',
-                    nargs=1,
-                    type=str,
-                    help='Input a constraint file')
+					'--constraint',
+					nargs=1,
+					type=str,
+					help='Input a constraint file')
+parser.add_argument('-g',
+					'--generate_constraint',
+					nargs='+',
+					type=str,
+					help='''generate constraint file. Two or three arguments are taken, 
+							first = PDB name, second = cst file name, third = weight,
+							if the third argument is not inputted, it will assume the weight to be 0.25''')
 parser.add_argument("-i", 
-                    '--input',
-                    nargs=1,
-                    type=str,
-                    required=True,
-                    help='input PDB file path/name')
+					'--input',
+					nargs=1,
+					type=str,
+					help='input PDB file path/name')
 parser.add_argument('-m',
-                    '--multi', 
+					'--multi', 
 					action='store_true',
-                    help='Relax multiple structures by multithreading')
+					help='Relax multiple structures by multithreading')
 parser.add_argument('-r',
-                    '--record',
-                    nargs=1,
-		    		type=str,
-                    help='Record a log file or not')
+					'--record',
+					nargs=1,
+					type=str,
+					help='Record a log file or not')
 
 
 args = parser.parse_args()
@@ -124,47 +159,56 @@ args = parser.parse_args()
 
 
 def main():
-    #----------------------------Start timer--------------------------------
-    start = time.time()
-    #----------------------------Main function------------------------------
-    if os.path.isfile(args.input[0]):
-        if args.constraint:
-            score = RunProcess(args.input[0], cst=args.constrain[0])
-        else:
-            score = RunProcess(args.input[0])
+	#----------------------------Start timer--------------------------------
+	start = time.time()
+	#----------------------------Main function------------------------------
 
-        if args.record:
-            with open(args.record[0], 'a') as fin:
-                fin.write(score)
-        
-        end = time.time()
-        # Calculate wall time
-        cal_wall_time(start, end)
+	if args.generate_constraint:
+		if len(args.generate_constraint) == 3:
+			gen_cst(args.generate_constraint[0], args.generate_constraint[1], int(args.generate_constraint[2]))
+		else:
+			gen_cst(args.generate_constraint[0], args.generate_constraint[1])
 
-    elif os.path.isdir(args.input[0]):
+	if args.input:
+		if os.path.isfile(args.input[0]):
+			if args.constraint:
+				score = RunProcess(args.input[0], cst=args.constraint[0])
+			else:
+				score = RunProcess(args.input[0])
 
-        PDBls = glob.glob(args.input[0]+'*.pdb')
+			if args.record:
+				with open(args.record[0], 'a') as fin:
+					fin.write(score)
+			
+			end = time.time()
+			# Calculate wall time
+			cal_wall_time(start, end)
 
-        pool = mp.Pool(int(mp.cpu_count()*2/3))
-        if args.constraint:
-            cstls = []
-            for _ in range(len(PDBls)):
-                cstls.append(args.constraint[0])
-            combine_ls = list(zip(PDBls,cstls))
-            scores = pool.map(RunProcess, combine_ls)
-        else:
-            scores = pool.map(RunProcess, PDBls)
+		elif os.path.isdir(args.input[0]):
 
-        if args.record:
-            with open(args.record[0], 'a') as fin:
-                fin.write(scores)
-        
+			PDBls = glob.glob(args.input[0]+'*.pdb')
 
-        end = time.time()
-        # Calculate wall time
-        cal_wall_time(start, end)
+			pool = mp.Pool(int(mp.cpu_count()*2/3))
+			if args.constraint:
+				cstls = []
+				for _ in range(len(PDBls)):
+					cstls.append(args.constraint[0])
+				combine_ls = list(zip(PDBls,cstls))
+				scores = pool.map(RunProcess, combine_ls)
+			else:
+				scores = pool.map(RunProcess, PDBls)
+
+			if args.record:
+				with open(args.record[0], 'a') as fin:
+					for i in scores:
+						fin.write(f'{i}\n')
+		
+
+		end = time.time()
+		# Calculate wall time
+		cal_wall_time(start, end)
 
 
 
 if __name__ == '__main__':
-    main()
+	main()
